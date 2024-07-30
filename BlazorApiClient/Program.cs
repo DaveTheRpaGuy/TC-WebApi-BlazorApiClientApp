8619e760befd9e6f7c2fb82c81c770e3c0ac8722
DaveTheRpaGuy <dave.morris.atl@gmail.com>
2024-07-30 13:43:24 -0400

    Add project files.

---
+++ BlazorApiClient/Program.cs
@@ -0,0 +1,29 @@
+using BlazorApiClient.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient("api", opts =>
+{
+    opts.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ApiUrl"));
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
